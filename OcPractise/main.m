d1555e406939d95e5a2a0194965af1e85352e895
Liu Yan <ly20050516@gmail.com>
2017-10-27 14:49:34 +0800

    Initial Commit

---
+++ OcPractise/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OcPractise
+//
+//  Created by LiuYan on 2017/10/27.
+//  Copyright © 2017年 LiuYan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
